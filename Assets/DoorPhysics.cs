1f3af6b8eafb26310d5d5bdba4a06846da337914
Epsiodes <episodes27@gmail.com>
2016-12-19 01:07:41 +1100

    Unity Demo

---
+++ Assets/DoorPhysics.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorPhysics : MonoBehaviour {
+
+    // F=μFN 
+    // Use this for initialization
+    [Header("简谐运动方程")]
+    [Header("角速度")]
+    public float omega = 0.2f;
+    public float A = 0.5f;
+    public float dt = 0;
+    public float phine = 20f;
+    
+
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float angle = A * Mathf.Cos(omega * dt + phine);
+        transform.eulerAngles = new Vector3(0,angle,0);
+        Debug.Log(angle);
+        dt++;
+
+	}
+}
